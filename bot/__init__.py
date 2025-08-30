fa6075782b29621dcdf94ec68ec5a6e3ec188434
erroratservice <110653823+erroratservice@users.noreply.github.com>
2025-08-30 19:37:55 +0530

    Create __init__.py

---
+++ bot/__init__.py
@@ -0,0 +1,13 @@
+"""
+Clean Media Indexing Bot
+Built for MediaInfo extraction and channel organization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Media Index Bot"
+
+# Package initialization
+from .core.config import Config
+from .core.client import TgClient
+
+__all__ = ['Config', 'TgClient']
