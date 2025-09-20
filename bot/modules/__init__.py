1a4e910f198e522120d93718ed450bbf304b1557
erroratservice <110653823+erroratservice@users.noreply.github.com>
2025-09-20 15:50:24 +0530

    Update __init__.py

--- bot/modules/__init__.py
+++ bot/modules/__init__.py
@@ -7,11 +7,13 @@ from .indexfiles import indexfiles_handler
 from .status import status_handler
 from .settings import settings_handler
 from .help import help_handler
+from .findencoders import findencoders_handler
 
 __all__ = [
     'updatemediainfo_handler',
     'indexfiles_handler', 
     'status_handler',
     'settings_handler',
-    'help_handler'
+    'help_handler',
+    'findencoders_handler'
 ]
